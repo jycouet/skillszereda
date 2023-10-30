e557a19b71e8e504b86f9550169ba40eb8cb32a5
jycouet <jycouet@gmail.com>
2023-10-30 10:53:05 +0100

    :art: NEW: ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>Skillszereda</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" />
+</svelte:head>
+
+<div class="content">
+	<slot />
+</div>
+
+<div class="footer">
+	<hr />
+	<em>© Dynamic Process {new Date().getFullYear()}</em>
+</div>
+
+<style>
+	.content {
+		flex: 1 0 auto;
+	}
+	.footer {
+		margin: 1rem;
+		flex-shrink: 0;
+	}
+</style>
